33176dcd8949d438763385e5e3735da8aa100594
rayb <biswajit.ray@netscout.com>
2021-07-15 14:15:52 +0530

    All remaingin code

--- src/coursera/quicksort/QuickSortMain.java
+++ src/coursera/quicksort/QuickSortMain.java
@@ -13,7 +13,7 @@ public class QuickSortMain {
         int[] b = new int[10000];
         int[] c = new int[10000];
         try {
-            BufferedReader br = new BufferedReader(new FileReader("QuickSort.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("..\\..\\QuickSort.txt"));
             String line= null;
             int i = 0;
             while( ( line = br.readLine()) != null )
@@ -30,21 +30,21 @@ public class QuickSortMain {
         }
         System.out.println("Array of length " + a.length);
     //    int[] a = {3,3,9 , 3,1,2 , 2,8,7 , 6,4,2, 2,0,0, 1,1,1};
-        long countA = quickSort(a , 0 , a.length  );
-        countA += a.length -1;
-        long countB = finalElementQuickSort(b , 0 , b.length - 1  );
-        countB += b.length -1;
-        long countC = medianQuickSort(c , 0 , c.length  );
-        countC += c.length -1;
+        long countA = quickSort(a , 0 , a.length - 1 );
+        //countA += a.length -1;
+        long countB = finalElementQuickSort(b , 0 , b.length -1 );
+       // countB += b.length -1;
+        long countC = medianQuickSort(c , 0 , c.length - 1 );
+        //countC += c.length -1; 
       /*  System.out.println();
         for (int i = 0; i < a.length; i++) {
             System.out.print(" "+ a[i] );
         }
 */
         System.out.println();
-        System.out.println("Inversion count A : " + countA);
-        System.out.println("Inversion count B : " + countB);
-        System.out.println("Inversion count C : " + countC);
+        System.out.println("Inversion count A :: " + countA);
+        System.out.println("Inversion count B :: " + countB);
+        System.out.println("Inversion count C :: " + countC);
 /*
         System.out.println();
         long finalEleCount = finalElementQuickSort(a , 0 , a.length  );
@@ -53,7 +53,7 @@ public class QuickSortMain {
     }
 
     private static long quickSort(int[] a , int start , int end){
-        long count = end -start -1;
+        long count = end -start ;
 
         if(end <= start)
             return 0;
@@ -76,69 +76,32 @@ public class QuickSortMain {
         a[ i - 1 ] = a[pivot];
         a[pivot] = temp;
 
-        count += quickSort(a,start , i - 1  );
+        count += quickSort(a,start , i - 1 );
         count += quickSort(a,i , end );
 
         return count;
     }
 
     private static long finalElementQuickSort(int[] a , int start , int end){
-        long count = end - start -1;
 
-        if(end <= start)
-            return 0;
+        int temp = a[start];
+        a[start] = a[end];
+        a[end] = temp;
 
-        int pivot =end;
-
-        int i = start;
-        int j;
-
-
-        for ( j = i ; j < end ; j++)
-        {
-            if(a[j] <= a[pivot] ) {
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-                i++;
-            }
-        }
-        int temp = a[i ];
-        a[ i  ] = a[pivot];
-        a[pivot] = temp;
-
-        count += finalElementQuickSort(a,start , i - 1   );
-        count += finalElementQuickSort(a,i  , end );
+        long count = quickSort(a , 0 , a.length - 1 );
 
         return count;
     }
 
-    private static long medianQuickSort(int[] a , int start , int end){
-        long count = end - start -1 ;
-
-        if(end <= start)
-            return count;
+    private static long medianQuickSort(int[] a , int start , int end){       
 
         int pivot = getPivot(a,start,end);
-
-        int i = pivot + 1;
-        int j;
-
-        for ( j = i ; j < end ; j++)
-        {
-            if(a[j] <= a[pivot] ) {
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-                i++;
-            }
-        }
-        int temp = a[i - 1];
-        a[ i - 1 ] = a[pivot];
+        int temp = a[start];
+        a[start] = a[pivot];
         a[pivot] = temp;
 
-        count += medianQuickSort(a,start , i - 1  );
-        count += medianQuickSort(a,i , end );
+        long count = quickSort(a , 0 , a.length - 1 );
+
         return count;
     }
 
